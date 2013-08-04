41e2f82e2fe7bb105e6f7a811856d8d30b4242be
tokkonopapa <tokkonopapa@yahoo.com>
2013-08-04 22:29:06 +0900

    Site updated at 2013-08-04 13:29:06 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -13,24 +13,16 @@ var github = (function(){
   return {
     showRepos: function(options){
       $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos"
-        , type: 'json'
+          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
+        , dataType: 'jsonp'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data) { return; }
-          for (var i = 0; i < data.length; i++) {
-            if (options.skip_forks && data[i].fork) { continue; }
-            repos.push(data[i]);
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
           }
-          repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
-
-            if (aDate === bDate) { return 0; }
-            return aDate > bDate ? -1 : 1;
-          });
-
           if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
         }
